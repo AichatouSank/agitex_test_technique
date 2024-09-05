b586b07aed0da53eef8bfae0ba4366c1f18d144f
Aichatou sankara <aichatousankara97@gmail.com>
2024-09-05 15:28:56 +0000

    initialisation du backend

---
+++ src/main/java/com/climax/rapport_statistique/RapportStatistiqueApplication.java
@@ -0,0 +1,13 @@
+package com.climax.rapport_statistique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapportStatistiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapportStatistiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/climax/rapport_statistique/RapportStatistiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climax.rapport_statistique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapportStatistiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
